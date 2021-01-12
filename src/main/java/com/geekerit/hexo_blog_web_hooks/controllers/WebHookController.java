98bdaf7f49983888c9eec05be0a520404cb0ffc1
geekerhub <geekerhub@126.com>
2021-01-12 18:28:36 +0800

    refactor: 提取本地代码仓库路径到配置文件

--- src/main/java/com/geekerit/hexo_blog_web_hooks/controllers/WebHookController.java
+++ src/main/java/com/geekerit/hexo_blog_web_hooks/controllers/WebHookController.java
@@ -1,6 +1,7 @@
 package com.geekerit.hexo_blog_web_hooks.controllers;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,31 +18,38 @@ import java.util.Arrays;
 @Slf4j
 public class WebHookController {
 
-    private static final String BASH = "/bin/bash";
-    private static final String TO_PARAM = "-c";
+    @Value("${docker.localRepositoryPath}")
+    private String localRepositoryPath;
+
+    private final String BASH = "/bin/bash";
+    private final String TO_PARAM = "-c";
     /**
      * <b>[Note]</b>
      * <p>1.git required set ssh_key otherwise need login github account after exec git pull command</p>
      * <p>2.replace dir path with yours github repository on your container</p>
      */
-    private static final String CHANGE_DIR = "git -C /root/blog_sources/HexoBlog/ pull";
-    private static final String[] COMMAND_ARRAY = new String[3];
-
-    static {
-        COMMAND_ARRAY[0] = BASH;
-        COMMAND_ARRAY[1] = TO_PARAM;
-        COMMAND_ARRAY[2] = CHANGE_DIR;
-    }
+    private final String CHANGE_DIR = "git -C " + localRepositoryPath + " pull";
+    private final String HEXO_CLEAN = "hexo clean";
+    private final String[] COMMAND_ARRAY = new String[4];
 
 
     @RequestMapping(value = "/webHooks")
     public void webHooks() {
         try {
-            Process exec = Runtime.getRuntime().exec(COMMAND_ARRAY);
+            String[] commandArray = buildCommandArray();
+            Process exec = Runtime.getRuntime().exec(commandArray);
             log.info("exec pull process info is {}", exec);
         } catch (IOException e) {
             log.warn("run web hooks throw exception and detail is {}", e.getMessage());
             e.printStackTrace();
         }
+    }
+
+    private String[] buildCommandArray() {
+        COMMAND_ARRAY[0] = BASH;
+        COMMAND_ARRAY[1] = TO_PARAM;
+        COMMAND_ARRAY[2] = CHANGE_DIR;
+        COMMAND_ARRAY[3] = HEXO_CLEAN;
+        return COMMAND_ARRAY;
     }
 }
